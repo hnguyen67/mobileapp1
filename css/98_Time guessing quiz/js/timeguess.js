12321ebf46a4627012d5fb2ab3377e7acf2e608c
hnguyen67 <nguyenthihuong672001@gmail.com>
2026-01-23 10:50:02 +0900

    Add files via upload

---
+++ css/98_Time guessing quiz/js/timeguess.js
@@ -0,0 +1,118 @@
+"use strict";
+
+
+const timer = document.getElementById("timer");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+let startTime;       // Startボタンクリック時の時刻
+let timeoutid;       // ID
+let stopTime = 0;    // Stopまでの経過時間
+
+// sound
+const startSound = new Audio("sound/start.mp3");
+const stopSound = new Audio("sound/stop1.mp3");
+const winSound = new Audio("sound/stop2.mp3");
+const resetSound = new Audio("sound/reset.mp3");
+
+// ボタンを"初期"状態とする
+setButtonStateInitial()
+
+////////////////////////
+// Startボタンクリック
+////////////////////////
+start.addEventListener("click",
+  function () {
+    // ボタンをタイマー"動作中"状態とする
+    setButtonStateRunning();
+    startTime = Date.now() - stopTime;
+    startSound.currentTime = 0;
+    startSound.play()
+    countUp();
+  }, false
+);
+
+////////////////////////
+// Stopボタンクリック
+////////////////////////
+stop.addEventListener("click",
+  function () {
+    // タイマーを"停止中"状態とする
+    setButtonStateStopped();
+    clearTimeout(timeoutid); //setTimeout()でセットしたタイマーを解除する際に使用
+    stopTime = Date.now() - startTime;
+    const seconds = stopTime / 1000;
+    if (seconds >= 10.00 && seconds < 11.00) {
+      winSound.currentTime = 0;
+      winSound.play();
+      document.querySelector(".wrapper").classList.add("fireworks-bg");
+    }
+    stopSound.currentTime = 0;
+    stopSound.play();
+  }, false
+);
+
+////////////////////////
+// Resetボタンクリック
+////////////////////////
+reset.addEventListener("click",
+  function () {
+    // ボタンを"初期"状態とする
+    setButtonStateInitial();
+    timer.textContent = "00:00.000";
+    stopTime = 0;
+    document.querySelector(".wrapper").classList.remove("fireworks-bg");
+    resetSound.currentTime = 0;
+    resetSound.play();
+  }
+);
+
+
+function countUp() {
+  const d = new Date(Date.now() - startTime + stopTime);
+  /* padStart()で２桁固定表示とする */
+  const m = String(d.getMinutes()).padStart(2, "0");
+  const s = String(d.getSeconds()).padStart(2, "0");
+  const ms = String(d.getMilliseconds()).padStart(3, "0");
+  /* 描画 */
+  timer.textContent = `${m}:${s}.${ms}`;
+
+  timeoutid = setTimeout(() => {
+    //再帰呼び出し
+    countUp();
+  }, 10);
+}
+
+// 初期 または Reset後
+function setButtonStateInitial() {
+  start.classList.remove("js-inactive");
+  stop.classList.add("js-inactive");
+  reset.classList.add("js-inactive");
+  start.classList.remove("js-unclickable");
+  stop.classList.add("js-unclickable");
+  reset.classList.add("js-unclickable");
+}
+
+// 状態:タイマー動作中
+function setButtonStateRunning() {
+  timer.classList.add("timer-fontColor_hidden"); //時間を見えなくする
+  start.classList.add("js-inactive");   // 非活性
+  stop.classList.remove("js-inactive");  // 活性
+  reset.classList.add("js-inactive");   // 非活性
+  start.classList.add("js-unclickable");
+  stop.classList.remove("js-unclickable");
+  reset.classList.add("js-unclickable");
+}
+
+// 状態:タイマー停止中
+function setButtonStateStopped() {
+  timer.classList.remove("timer-fontColor_hidden"); //時間を見えるようにする
+  timer.classList.add("timer_appear"); //時間をゆっくり表示
+  start.classList.add("js-inactive"); // 活性
+  stop.classList.add("js-inactive");    // 非活性
+  reset.classList.remove("js-inactive"); // 活性
+  start.classList.add("js-unclickable");
+  stop.classList.add("js-unclickable");
+  reset.classList.remove("js-unclickable");
+}
